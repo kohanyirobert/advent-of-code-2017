2f79db325fd4992a5bd0a4254a28913aaf33d2c5
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-05 16:37:14 +0000

    Day 1 - refactor, add solutions

--- 01/Part1.hs
+++ 01/Part1.hs
@@ -2,6 +2,6 @@ import Day1
 
 main = do
   string <- getLine
-  let number = read string :: Integer
+  let number = read string
   let digits = toDigits number
   print $ sumEqualDigitsN digits 1 0
--- 01/Part2.hs
+++ 01/Part2.hs
@@ -2,8 +2,8 @@ import Day1
 
 main = do
   string <- getLine
-  let number = read string :: Integer
+  let number = read string
   let digits = toDigits number
   let size = length digits
   let halfSize = size `div` 2
-  print (sumEqualDigitsN digits halfSize 0)
+  print $ sumEqualDigitsN digits halfSize 0
