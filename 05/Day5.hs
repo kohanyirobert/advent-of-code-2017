3961a2a7570c66658ce1b15bfab5cf6441c3b985
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-05 13:48:20 +0000

    Day 5 - Part 2 done

--- 05/Day5.hs
+++ 05/Day5.hs
@@ -1,9 +1,27 @@
 module Day5 where
 
+import Data.Sequence
+
 type Step = Int
 type Position = Int
 type Jump = Int
-type Jumps = [Jump]
+type Jumps = Seq Jump
+type Offset = Int
+type OffsetFunction = (Offset -> Offset)
 
 getJumps :: String -> Jumps
-getJumps string = map read $ lines string
+getJumps string = fromList $ map read $ lines string
+
+followJumps' :: OffsetFunction -> Step -> Position -> Jumps -> Step
+followJumps' func step position jumps
+  | newPosition < 0 || newPosition > size - 1 = nextStep
+  | otherwise = followJumps' func nextStep newPosition newJumps
+  where size = Data.Sequence.length jumps
+        offset = index jumps position
+        newOffset = func offset
+        nextStep = step + 1
+        newPosition = position + offset
+        newJumps = update position newOffset jumps
+
+followJumps :: OffsetFunction -> Jumps -> Step
+followJumps func jumps = followJumps' func 0 0 jumps
---
+++ 05/Part2.hs
@@ -0,0 +1,8 @@
+import Day5
+
+main = do
+  string <- getContents
+  let jumps = getJumps string
+  let func = \x -> x + if x >= 3 then -1 else 1
+  let step = followJumps func jumps
+  print step
