d651878fd04188322e822897968240f298ae1b47
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-07 17:07:10 +0000

    Day 7 - Part 1 refactor

--- 07/Day7.hs
+++ 07/Day7.hs
@@ -6,22 +6,31 @@ import Data.List (partition)
 data Detail = Detail String Int
   deriving Show
 
-type Tower = (Detail, [String])
+data Tower = Tower Detail [String]
+  deriving Show
+
 type Towers = [Tower]
+type Nodes = [Node]
 
 getTowers :: String -> Towers
 getTowers string = map toTower dataRecords
   where toDataLine = \x -> filter (\y -> isAlphaNum y || isSpace y && not (isControl y)) x
         dataLines = map toDataLine $ lines string
         dataRecords = map words dataLines
-        toTower = \(name : weight : children) -> (Detail name (read weight), children)
+        toTower = \(name : weight : children) -> Tower (Detail name (read weight)) children
 
 isRoot :: Tower -> Towers -> Bool
-isRoot (Detail name _, _) towers = all (\(_, children) -> not (name `elem` children)) towers
+isRoot (Tower (Detail name _) _) towers = all (\(Tower _ children) -> not (name `elem` children)) towers
 
 findRoot :: Towers -> Tower
 findRoot towers = head $ filter (\x -> isRoot x towers) towers
 
 partitionTowers :: Towers -> (Towers, Towers)
 partitionTowers towers = (nodes, leafs)
-  where (leafs, nodes) = partition (\x@(_, children) -> children == []) towers
+  where (leafs, nodes) = partition (\(Tower _ children) -> children == []) towers
+
+findParent :: Tower -> Nodes -> Maybe Node
+findParent _ [] = Nothing
+findParent tower@(Tower (Detail target _) _) (node@(Node (Detail name _) _) : nodes)
+  | target == name = Just node
+  | otherwise = findParent tower nodes
--- 07/Part1.hs
+++ 07/Part1.hs
@@ -4,5 +4,5 @@ main = do
   string <- getContents
   let towers = getTowers string
   let (nodes, _) = partitionTowers towers
-  let (Detail name _, _) = findRoot nodes
+  let Tower (Detail name _) _ = findRoot nodes
   print name
