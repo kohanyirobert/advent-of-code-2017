230a2c79a96db2249d2cac8780b5b52079d29ae6
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-21 20:25:19 +0000

    Day 12 - Part 1 done

--- 12/Part1.hs
+++ 12/Part1.hs
@@ -1,5 +1,11 @@
 import Day12
 
+import Data.Maybe (fromJust)
+
 main = do
   string <- getContents
-  print string
+  let programs = getPrograms string
+  let targetProgram = fromJust $ findProgram 0 programs
+  let node = buildGraph targetProgram programs
+  let ids = collectIds node
+  print $ length ids
