3c8a643b2595a2c0bd1236d6ee1f5243f8137e44
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-22 20:55:23 +0000

    Day 13 - Part 1 done

---
+++ 13/Day13.hs
@@ -0,0 +1,62 @@
+module Day13 where
+
+import Data.Char (isDigit, isSpace)
+
+type Depth = Int
+type Range = Int
+type Severity = Int
+type Picosecond = Int
+type Position = Int
+
+data Direction = Up | Down
+  deriving Show
+
+data Scanner = None | Scanner Position Direction
+  deriving Show
+
+data Layer = Layer Depth Range Severity Scanner
+  deriving Show
+
+fillMissingLayers :: [Layer] -> [Layer]
+fillMissingLayers [] = []
+fillMissingLayers (layer : []) = [layer]
+fillMissingLayers (layer@(Layer depth _ _ _) : layers@(Layer nextDepth _ _ _ : _))
+  | depth + 1 == nextDepth = layer : fillMissingLayers layers
+  | otherwise = layer : fillMissingLayers (missingLayer : layers)
+  where missingLayer = Layer (depth + 1) 0 0 None
+
+getLayers :: String -> [Layer]
+getLayers string = allLayers
+  where keepNumbers = \line -> filter (\char -> isDigit char || isSpace char) line
+        dataLines = map keepNumbers $ lines string
+        toLayer = \(d : r : []) -> let depth = read d
+                                       range = read r
+                                       severity = depth * range
+                                   in Layer depth range severity (Scanner 0 Down)
+        layers = map toLayer $ map words dataLines
+        allLayers = fillMissingLayers layers
+
+moveScanner :: Range -> Scanner -> Scanner
+moveScanner _ None = None
+moveScanner range (Scanner position Down)
+  | position == range - 1 = Scanner (position - 1) Up
+  | otherwise = Scanner (position + 1) Down
+moveScanner range (Scanner position Up)
+  | position == 0 = Scanner (position + 1) Down
+  | otherwise = Scanner (position - 1) Up
+
+ridePacket' :: Picosecond -> [Layer] -> [Layer] -> [Layer]
+ridePacket' picosecond layers triggeredLayers
+  | picosecond == size = triggeredLayers
+  | otherwise = ridePacket' nextPicosecond nextLayers nextTriggeredLayers
+  where size = length layers
+        layer = layers !! picosecond
+        nextTriggeredLayers = case layer
+                              of (Layer _ _ _ (Scanner 0 _)) -> triggeredLayers ++ [layer]
+                                 _ -> triggeredLayers
+        nextPicosecond = picosecond + 1
+        updateLayer = \(Layer depth range severity scanner) -> Layer depth range severity (moveScanner range scanner)
+        nextLayers = map updateLayer layers
+
+ridePacket :: [Layer] -> [Layer]
+ridePacket layers = ridePacket' 0 layers []
---
+++ 13/Part1.hs
@@ -0,0 +1,8 @@
+import Day13
+
+main = do
+  string <- getContents
+  let layers = getLayers string
+  let triggeredLayers = ridePacket layers
+  let totalSeverity = sum $ map (\(Layer _ _ severity _) -> severity) triggeredLayers
+  print totalSeverity
