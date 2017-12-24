0479f0446cc7707d53e252e8c49caecce1d196cc
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-24 14:02:24 +0000

    Day 14 - Part 2 refactor

--- 14/Day14.hs
+++ 14/Day14.hs
@@ -5,21 +5,27 @@ import qualified Data.Map.Strict as Map
 import Data.Char (isControl)
 import Day10 (Encoding(Binary), knotHash)
 
-data State = Free | Used
+type Region = Int
+
+data State = Free | Used Region
   deriving (Eq, Show)
 
 type Count = Int
 type Coodinate = (Int, Int)
 type Grid = Map.Map Coodinate State
 
+isUsed :: State -> Bool
+isUsed Free = False
+isUsed (Used _) = True
+
 getGrid :: String -> Grid
 getGrid string = Map.fromList $ concat indexedGrid
   where range = [0..127]
         key = filter (not . isControl) string
         binaryHashes = map (\i -> knotHash Binary (key ++ "-" ++ show i)) range
-        toRow = \binaryHash -> map (\char -> if char == '0' then Free else Used) binaryHash
+        toRow = \binaryHash -> map (\char -> if char == '0' then Free else Used 0) binaryHash
         grid = map toRow binaryHashes
         indexedGrid = map (\(i, row) -> map (\(j, state) -> ((i, j), state)) $ zip range row) $ zip range grid
 
-countState :: State -> Grid -> Count
-countState state grid = foldl (\count s -> count + if s == state then 1 else 0) 0 grid
+countUsed :: Grid -> Count
+countUsed grid = foldl (\count s -> count + if isUsed s then 1 else 0) 0 grid
--- 14/Part1.hs
+++ 14/Part1.hs
@@ -3,4 +3,4 @@ import Day14
 main = do
   string <- getContents
   let grid = getGrid string
-  print $ countState Used grid
+  print $ countUsed grid
