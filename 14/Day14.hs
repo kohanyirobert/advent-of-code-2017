217837931e0b54f862ea7907dadff791cd43dbab
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-24 13:57:36 +0000

    Day 14 - Part 2 refactor to use map

--- 14/Day14.hs
+++ 14/Day14.hs
@@ -1,23 +1,25 @@
 module Day14 where
 
-import Day10 (Encoding(Binary), knotHash)
+import qualified Data.Map.Strict as Map
 
 import Data.Char (isControl)
+import Day10 (Encoding(Binary), knotHash)
 
 data State = Free | Used
   deriving (Eq, Show)
 
 type Count = Int
-type Grid = [[State]]
+type Coodinate = (Int, Int)
+type Grid = Map.Map Coodinate State
 
 getGrid :: String -> Grid
-getGrid string = grid
-  where key = filter (not . isControl) string
-        binaryHashes = map (\i -> knotHash Binary (key ++ "-" ++ show i)) [0..127]
+getGrid string = Map.fromList $ concat indexedGrid
+  where range = [0..127]
+        key = filter (not . isControl) string
+        binaryHashes = map (\i -> knotHash Binary (key ++ "-" ++ show i)) range
         toRow = \binaryHash -> map (\char -> if char == '0' then Free else Used) binaryHash
         grid = map toRow binaryHashes
+        indexedGrid = map (\(i, row) -> map (\(j, state) -> ((i, j), state)) $ zip range row) $ zip range grid
 
 countState :: State -> Grid -> Count
-countState state grid = 
-  let countRow =  \row -> foldl (\count digit -> count + if digit == state then 1 else 0) 0 row
-  in foldl (\count row -> count + countRow row) 0 grid
+countState state grid = foldl (\count s -> count + if s == state then 1 else 0) 0 grid
