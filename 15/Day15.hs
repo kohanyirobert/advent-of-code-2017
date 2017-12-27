45f91e6e82aa69772071654f0fe57d28f9388d84
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-27 20:11:49 +0000

    Day 15 - Part 2 refactor

--- 15/Day15.hs
+++ 15/Day15.hs
@@ -5,6 +5,13 @@ import Data.Bits ((.&.))
 
 type Pair = (Int, Int)
 
+divisor :: Int
+divisor = 2147483647
+
+factorA, factorB :: Int
+factorA = 16807
+factorB = 48271
+
 getFirstPair :: String -> Pair
 getFirstPair string = (a, b)
   where (a : b : _) = map (read . filter isDigit) $ lines string
@@ -12,10 +19,8 @@ getFirstPair string = (a, b)
 isMatch:: Pair -> Bool
 isMatch (a, b) = a .&. 0xffff == b .&. 0xffff
 
-generate :: Int -> Int -> [Int]
-generate factor start = iterate (\next -> next * factor `mod` 2147483647) start
+generator :: Int -> Int -> [Int]
+generator factor start = iterate (\next -> next * factor `mod` divisor) start
 
-getPairs :: Int -> Pair -> [Pair]
-getPairs n (a, b) = take n $ zip as bs
-  where as = generate 16807 a
-        bs = generate 48271 b
+getPairs :: Int -> [Int] -> [Int] -> [Pair]
+getPairs n as bs = take n $ zip as bs
--- 15/Part1.hs
+++ 15/Part1.hs
@@ -2,7 +2,9 @@ import Day15
 
 main = do
   string <- getContents
-  let firstPair = getFirstPair string
-  let pairs = getPairs 40000000 firstPair
+  let (firstA, firstB) = getFirstPair string
+  let generatorA = generator factorA firstA
+  let generatorB = generator factorB firstB
+  let pairs = getPairs 40000000 generatorA generatorB
   let matches = filter isMatch pairs
   print $ length matches
