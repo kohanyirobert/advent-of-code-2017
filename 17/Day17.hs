c5c436e43d7ca46d13ab054a2b50b63b189905bd
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-12-30 13:13:54 +0000

    Day 17 - Part 2 done

--- 17/Day17.hs
+++ 17/Day17.hs
@@ -1,20 +1,52 @@
 module Day17 where
 
+import Debug.Trace (traceShow)
+
+type Count = Int
 type StepSize = Int
 type Value = Int
 type Position = Int
-type Buffer = [Int]
+type Size = Int
+
+data Buffer = CircularBuffer Size [Value] | ValueBuffer Size Position (Maybe Value)
+  deriving Show
 
 data Spinlock = Spinlock StepSize Position Value Buffer
   deriving Show
 
-newSpinlock :: StepSize -> Spinlock
-newSpinlock stepSize = Spinlock stepSize 0 0 [0]
+makeSpinlock :: Buffer -> StepSize -> Spinlock
+makeSpinlock buffer stepSize = Spinlock stepSize 0 0 buffer
 
-stepSpinlock :: Spinlock -> Spinlock
-stepSpinlock (Spinlock stepSize position value buffer) = Spinlock stepSize nextPosition nextValue newBuffer
-  where nextValue = value + 1
-        bufferSize = length buffer
-        nextPosition = ((position + stepSize) `mod` bufferSize) + 1
-        (bufferHead, bufferTail) = splitAt nextPosition buffer
-        newBuffer = bufferHead ++ [nextValue] ++ bufferTail
+makeCircularBuffer :: Buffer
+makeCircularBuffer = CircularBuffer 1 [0]
+
+makeValueBuffer :: Position -> Buffer
+makeValueBuffer position = ValueBuffer 1 position Nothing
+
+updateBuffer :: Position -> Value -> Buffer -> Buffer
+updateBuffer position value (CircularBuffer size values) = newBuffer
+  where (valuesHead, valuesTail) = splitAt position values
+        newValues = valuesHead ++ [value] ++ valuesTail
+        nextSize = size + 1
+        newBuffer = CircularBuffer nextSize newValues
+updateBuffer position value (ValueBuffer size trackedPosition trackedValue)
+  | position == trackedPosition = ValueBuffer nextSize trackedPosition (Just value)
+  | otherwise = ValueBuffer nextSize trackedPosition trackedValue
+  where nextSize = size + 1
+
+bufferSize :: Buffer -> Int
+bufferSize (CircularBuffer size _) = size
+bufferSize (ValueBuffer size _ _) = size
+
+stepSpinlock :: Count -> Spinlock -> Spinlock
+stepSpinlock count spinlock@(Spinlock stepSize position value buffer)
+  | count == 0 = spinlock
+  | otherwise = stepSpinlock nextCount $ if count `mod` 5000 == 0
+                                         then traceShow newSpinlock newSpinlock
+                                         else newSpinlock
+  where size = bufferSize buffer
+        nextCount = count - 1
+        nextPosition = ((position + stepSize) `mod` size) + 1
+        nextValue = value + 1
+        newBuffer = updateBuffer nextPosition nextValue buffer
+        newSpinlock = Spinlock stepSize nextPosition nextValue newBuffer
--- 17/Part1.hs
+++ 17/Part1.hs
@@ -3,6 +3,6 @@ import Day17
 main = do
   string <- getContents
   let stepSize = read string
-  let spinlock = newSpinlock stepSize
-  let (Spinlock _ position _ buffer) = iterate stepSpinlock spinlock !! 2017
-  print $ buffer !! (position + 1)
+  let spinlock = makeSpinlock (makeCircularBuffer) stepSize
+  let (Spinlock _ position _ (CircularBuffer _ values)) = stepSpinlock 2017 spinlock
+  print $ values !! (position + 1)
