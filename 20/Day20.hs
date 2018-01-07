4ca9f89edc7598570c5bf2cc14a886cd8c81320b
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-01-07 19:22:05 +0000

    Day 20 - Part 1 done

--- 20/Day20.hs
+++ 20/Day20.hs
@@ -1,1 +1,38 @@
 module Day20 where
+
+import Data.Char (isDigit)
+
+type Vector = (Int, Int, Int)
+
+data Particle = Particle
+  { position :: Vector
+  , velocity :: Vector
+  , acceleration :: Vector
+  } deriving (Eq, Show)
+
+instance Ord Particle where
+  a `compare` b = origoDistance a `compare` origoDistance b
+
+toVector :: String -> Vector
+toVector string
+  = let xyz = map read
+            . words
+            . map (\c -> if c == ',' then ' ' else c)
+            . filter (\c -> isDigit c || c == '-' || c == ',')
+            $ string
+    in (xyz !! 0, xyz !! 1, xyz !! 2)
+
+toParticle :: [String] -> Particle
+toParticle (p : v : a : []) = Particle (toVector p) (toVector v) (toVector a)
+
+getParticles string
+  = map toParticle
+  . map words
+  . lines
+  $ string
+
+origoDistance :: Particle -> Int
+origoDistance (Particle _ _ (x, y, z)) = abs x + abs y + abs z
+
+closestParticle :: [Particle] -> Particle
+closestParticle particles = minimum particles
--- 20/Part1.hs
+++ 20/Part1.hs
@@ -1,1 +1,10 @@
 import Day20
+
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+
+main = do
+  string <- getContents
+  let particles = getParticles string
+      closest = closestParticle particles
+  print $ fromJust $ closest `elemIndex` particles
